43ebe453397983422e57cce4adb62902b22e1f77
nyassky <gswarrios@gmail.com>
2026-01-13 18:32:24 +0300

    final

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,8 @@
 import { createAction } from "@reduxjs/toolkit";
 import City from "../types/cities";
-import Offer from "../types/offer";
+import Offer from "../types/offer"; 
 
 export const changeCity = createAction<City>('city/CHANGE_CITY');
 export const fillingList = createAction<Offer[]>('city/FILLING_LIST');
-export const changeSorting = createAction<string>('sort/CHANGE_SORTING');
+export const changeSorting = createAction<string>('sort/CHANGE_SORTING'); 
  
