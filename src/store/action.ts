f433579e47f2a56ed3a8637afd3462f8dd3a7bed
nyassky <gswarrios@gmail.com>
2026-01-13 18:30:17 +0300

    .

--- src/store/action.ts
+++ src/store/action.ts
@@ -4,5 +4,5 @@ import Offer from "../types/offer";
 
 export const changeCity = createAction<City>('city/CHANGE_CITY');
 export const fillingList = createAction<Offer[]>('city/FILLING_LIST');
-export const changeSorting = createAction<string>('sort/CHANGE_SORTING'); 
+export const changeSorting = createAction<string>('sort/CHANGE_SORTING');
  
